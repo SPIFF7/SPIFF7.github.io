688943583e373dfae9a620c432871fa905489cef
Mário Abrantes <spiff777@gmail.com>
2022-01-07 19:19:17 +0000

    Mobile v1

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,20 @@
-//adicionar links fazerem zoom
-
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 }
 
 let startingHeight = window.innerHeight;
+let startingWidth = window.innerWidth;
 document.body.style.height = startingHeight*2 + "px";
 
 window.onscroll = function (){
-    pageScroll();
+    if(startingWidth > 768){
+        pageScrollDesktop();
+    } else {
+        pageScrollMobile();
+    }
 };
 
-function pageScroll(){
+function pageScrollDesktop(){
     if (document.documentElement.scrollTop < 400) {
         //#greet zooms in
         document.getElementById("hi").style.fontSize = "250px";
@@ -53,6 +56,31 @@ function pageScroll(){
     }
 }
 
-/*function zoomText(element, size){
-    element.style.fontSize = size;
-}*/
+function pageScrollMobile(){
+    if (document.documentElement.scrollTop < 400) {
+        //#greet zooms in
+        document.getElementById("hi").style.fontSize = "150px";
+        document.getElementById("greet").style.fontSize = "40px";
+        document.getElementById("greet").style.height = "100%";
+        document.getElementById("socials").style.top = "100%";
+        document.getElementById("socials").style.height = "0%";
+        document.getElementById("contacts").style.top = "100%";
+        document.getElementById("contacts").style.height = "0%";
+    } else if(document.documentElement.scrollTop > 400){
+        //#socials and #contacts zoom in
+        document.getElementById("hi").style.fontSize = "16px";
+        document.getElementById("greet").style.fontSize = "16px";
+        document.getElementById("greet").style.height = "25%";
+        document.getElementById("socials").style.fontSize = "30px";
+        document.getElementById("gitIcon").style.width = "100px";
+        document.getElementById("twitterIcon").style.width = "100px";
+        document.getElementById("linkedinIcon").style.width = "100px";
+        document.getElementById("socials").style.top = "25%";
+        document.getElementById("socials").style.height = "35%";
+        document.getElementById("socials").style.visibility = "visible";
+        document.getElementById("contacts").style.fontSize = "30px";
+        document.getElementById("contacts").style.top = "60%";
+        document.getElementById("contacts").style.height = "40%";
+        document.getElementById("contacts").style.visibility = "visible";
+    }
+}
